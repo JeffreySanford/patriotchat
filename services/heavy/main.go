0a6a22c1fb953127625c28b1435133dd86cc2a23
Jeffrey <jeffreysanford@gmail.com>
2026-01-31 15:47:04 -0600

    chore(heavy): accept --port / PORT env, default 4000

--- services/heavy/main.go
+++ services/heavy/main.go
@@ -1,8 +1,10 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net/http"
+	"os"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -10,7 +12,21 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func main() {
+	// Determine port from --port flag or PORT env var, default to 4000
+	portFlag := flag.String("port", "", "port to listen on")
+	flag.Parse()
+	port := "4000"
+	if *portFlag != "" {
+		port = *portFlag
+	} else if env := os.Getenv("PORT"); env != "" {
+		port = env
+	}
+	addr := ":" + port
+
 	http.HandleFunc("/health", handler)
-	fmt.Println("heavy service listening on :8080")
-	http.ListenAndServe(":8080", nil)
+	fmt.Printf("heavy service listening on %s\n", addr)
+	if err := http.ListenAndServe(addr, nil); err != nil {
+		fmt.Fprintf(os.Stderr, "failed to start heavy service: %v\n", err)
+		os.Exit(1)
+	}
 }
